c5adb2016758e1e41b181fd93ccdb543a289c668
Brandon Newton <bitwiselove@gmail.com>
2016-03-07 15:27:09 -0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+
+module.exports = {
+  entry: ['./src/index'],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        exclude: /node_modules/
+        loader: 'babel-loader',
+      }
+    ]
+  }
+};
