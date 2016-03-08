33e337b3a3dbb730b3bd11c3703d8047c518c4ad
Brandon Newton <bitwiselove@gmail.com>
2016-03-07 23:20:50 -0800

    Tomato styling

--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ const FormatedTimestamp = ({
   const minutes = parseInt(totalSeconds / 60, 10);
   const format = time => time < 10 ? '0' + time : time;
 
-  return <span>{`${format(minutes)}:${format(seconds)}`}</span>
+  return <span className="countdown">{`${format(minutes)}:${format(seconds)}`}</span>
 };
 
 const StartButton = ({
   onClick
 }) => (
-  <button onClick={onClick}>Start Timer</button>
+  <button className="button button--primary" onClick={onClick}>Start Timer</button>
 );
 
 const PlayPauseButton = ({
@@ -26,7 +26,7 @@ const PlayPauseButton = ({
   let action = ticking ? onPause : onPlay;
   let label = ticking ? 'Pause Timer' : 'Resume Timer';
 
-  return <button onClick={action}>{label}</button>;
+  return <button className="button button--primary" onClick={action}>{label}</button>;
 }
 
 class CountdownTimer extends React.Component {
@@ -36,6 +36,8 @@ class CountdownTimer extends React.Component {
     this.resume = this.resume.bind(this);
     this.reset = this.reset.bind(this);
     this.tick = this.tick.bind(this);
+    this.takeBreak = this.takeBreak.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);
     this.state = {
       timeRemaining: props.initialTimeInMs,
       previousTime: null,
@@ -73,21 +75,37 @@ class CountdownTimer extends React.Component {
     });
 
     if (countdownComplete) {
-      this.props.onComplete()
+      if (!this.state.takingBreak)
+        this.props.onComplete()
+
       clearTimeout(this.state.timeout);
       this.setState({completed: true});
     }
   }
 
   reset() {
+    this.resetTimer(this.props.initialTimeInMs, () => {
+      this.setState({
+        takingBreak: false
+      }, this.tick);
+    });
+  }
+
+  resetTimer(milliseconds, cb) {
     clearTimeout(this.state.timeout);
     this.setState({
-      timeRemaining: this.props.initialTimeInMs,
+      timeRemaining: milliseconds,
       previousTime: null,
       timeout: null,
       ticking: false,
       completed: false
-    })
+    }, cb)
+  }
+
+  takeBreak() {
+    this.setState({
+      takingBreak: true,
+    }, this.resetTimer(this.props.breakLength, this.tick));
   }
 
   resume() {
@@ -102,8 +120,13 @@ class CountdownTimer extends React.Component {
   }
 
   render() {
+    var BreakButton = null;
+
     if (this.state.completed) {
-      var ControlButton = <button onClick={this.reset}>Reset Timer</button>;
+      var ControlButton = <button className="button button--primary" onClick={this.reset}>Reset Timer</button>;
+      if (!this.state.takingBreak) {
+        var BreakButton = <button className="button button--cool" onClick={this.takeBreak}>Take a Break</button>;
+      }
     } else {
       var ControlButton = this.state.previousTime === null ? <StartButton onClick={this.resume} /> : <PlayPauseButton
         ticking={this.state.ticking}
@@ -114,10 +137,13 @@ class CountdownTimer extends React.Component {
 
     return (
       <div>
-        {' '}
-        <FormatedTimestamp milliseconds={this.state.timeRemaining} />
-        {' '}
-        {ControlButton}
+        <div className="text-center">
+          <FormatedTimestamp milliseconds={this.state.timeRemaining} />
+        </div>
+        <div className="text-center">
+          {ControlButton}
+          {BreakButton}
+        </div>
       </div>
     );
   }
@@ -126,27 +152,55 @@ class CountdownTimer extends React.Component {
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.tomatoTime = 1000 * 60 * 25;
+    this.smallBreak = 1000 * 60 * 5;
+    this.largeBreak = 1000 * 60 * 25;
     this.onComplete = this.onComplete.bind(this);
     this.state = {
-      tomatos: 0
+      totalTomatos: 0,
+      breakCounter: 0,
+      breakLength: this.smallBreak,
     }
   }
 
   onComplete() {
-    this.setState({
-      tomatos: this.state.tomatos + 1
-    });
+    if ((this.state.breakCounter + 1) < 4) {
+      this.setState({
+        totalTomatos: this.state.totalTomatos + 1,
+        breakCounter: this.state.breakCounter + 1,
+        breakLength: this.smallBreak,
+      });
+    } else {
+      this.setState({
+        totalTomatos: this.state.totalTomatos + 1,
+        breakCounter: 0,
+        breakLength: this.largeBreak,
+      });
+    }
   }
 
   render() {
+    var tomatos = [];
+    for (var i = 0; i < this.state.totalTomatos; i++) {
+      tomatos.push(<img src="http://emojipedia-us.s3.amazonaws.com/cache/89/dd/89dd52458d0d4c147b56bfa316ce5fe2.png" className="icon-small" />);
+    }
+    tomatos = tomatos.length > 0 ? tomatos : <img src="http://emojipedia-us.s3.amazonaws.com/cache/2c/e1/2ce12e2adad77ef4248afab9c094f08e.png" className="icon-small" />
+
+
     return (
       <div>
-        <h1>Tomato</h1>
-        <CountdownTimer
-          initialTimeInMs={5000}
-          onComplete={this.onComplete}
-        />
-        <p>{this.state.tomatos}</p>
+        <div className="container">
+          <h1><img src="http://emojipedia-us.s3.amazonaws.com/cache/89/dd/89dd52458d0d4c147b56bfa316ce5fe2.png"/></h1>
+          <CountdownTimer
+            initialTimeInMs={this.tomatoTime}
+            breakLength={this.state.breakLength}
+            onComplete={this.onComplete}
+          />
+          <div className="flex-vertical">
+            <span className="completed-text">Tomatos completed:</span> {tomatos}
+          </div>
+        </div>
+        <p className="credits">Created by <a href="http://forkbombs.com" target="_blank">Brandon Newton</a>.</p>
       </div>
     );
   }
